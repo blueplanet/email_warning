d0650153ebc0bb271554eafb7914c0222fa437e3
blueplanet <erguolinge@gmail.com>
2015-12-08 06:29:27 +0900

    Try for staging

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  resources :users do
+    resources :blogs do
+      resources :posts do
+        resources :comments
+      end
+    end
+  end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
